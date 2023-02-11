ee652fd013f64bcc2a4a91f829bb13c439f8b7f3
instamenta <98179343+instamenta@users.noreply.github.com>
2023-02-11 23:58:37 +0200

    3D object visible, light init and positioned

--- 3DPlanetSpin/main.js
+++ 3DPlanetSpin/main.js
@@ -4,20 +4,21 @@ import './style.css'
 const scene = new THREE.Scene();
 
 const geometry = new THREE.SphereGeometry(3,64, 64)
-
 const material = new THREE.MeshStandardMaterial({
   color: "#00ff83",
 })
 
 const mesh = new THREE.Mesh(geometry, material)
-
 scene.add(mesh)
 
-const camera = new THREE.PerspectiveCamera(45, 800/600)
+const light = new THREE.PointLight(0xffffff, 1 ,100)
+light.position.set(0, 10, 10)
+scene.add(light)
+const camera = new THREE.PerspectiveCamera(45, 800 / 600)
+camera.position.z = 10
 scene.add(camera)
 
-const canvas = document.querySelector('.webgl')
-
+const canvas = document.querySelector(".webgl")
 const renderer = new THREE.WebGLRenderer({canvas})
 
 renderer.setSize(800, 600)
