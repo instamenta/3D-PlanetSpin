6eb6b59926abba38ebc778ea2585e2a48e63e3d9
instamenta <98179343+instamenta@users.noreply.github.com>
2023-02-12 00:02:21 +0200

    responsive design basic implementation

--- 3DPlanetSpin/main.js
+++ 3DPlanetSpin/main.js
@@ -11,15 +11,23 @@ const material = new THREE.MeshStandardMaterial({
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+const sizes = { 
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
 const light = new THREE.PointLight(0xffffff, 1 ,100)
 light.position.set(0, 10, 10)
 scene.add(light)
-const camera = new THREE.PerspectiveCamera(45, 800 / 600)
+
+
+
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height)
 camera.position.z = 10
 scene.add(camera)
 
 const canvas = document.querySelector(".webgl")
 const renderer = new THREE.WebGLRenderer({canvas})
 
-renderer.setSize(800, 600)
+renderer.setSize(sizes.width , sizes.height)
 renderer.render(scene, camera)
